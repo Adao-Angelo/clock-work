ba262e0cc3c8854275d46b4065bf9542f3c26d20
Adao-Angelo <adaobegginer@gmail.com>
2024-12-12 23:55:18 +0100

    add taileind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <main className="container"></main>;
+  return (
+    <main className="bg-black w-full h-[100vh] text-zinc-50">
+      <h1>19:00</h1>
+    </main>
+  );
 }
 
 export default App;
