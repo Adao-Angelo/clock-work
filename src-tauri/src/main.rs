f087049cefb58ed78e34f9a885a4193351f2b635
Adao-Angelo <adaobegginer@gmail.com>
2024-12-16 23:32:26 +0100

    add store on app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,11 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri_plugin_store;
 
 fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::default().build()) 
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
     clock_work_lib::run()
 }
